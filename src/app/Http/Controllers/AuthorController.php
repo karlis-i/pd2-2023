f439526e82e9e5695df2d10178846491fabc8b5e
Kārlis Immers <karlis.im@gmail.com>
2023-05-20 12:40:12 +0300

    Pabeigta Autoru sadaļa

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -20,4 +20,62 @@ class AuthorController extends Controller
             ]
         );
     }
+
+    // display new author form
+    public function create()
+    {
+        return view(
+            'author.form',
+            [
+                'title' => 'Pievienot jaunu autoru',
+                'author' => new Author(),
+            ]
+        );
+    }
+
+    // save new author
+    public function put(Request $request)
+    {
+        $validatedDate = $request->validate([
+            'name' => 'required',
+        ]);
+
+        $author = new Author();
+        $author->name = $validatedDate['name'];
+        $author->save();
+
+        return redirect('/authors');
+    }
+
+    // display author update form
+    public function update(Author $author)
+    {
+        return view(
+            'author.form',
+            [
+                'title' => 'Rediģēt autoru',
+                'author' => $author,
+            ]
+        );
+    }
+
+    // update existing authors
+    public function patch(Author $author, Request $request)
+    {
+        $validatedDate = $request->validate([
+            'name' => 'required',
+        ]);
+
+        $author->name = $validatedDate['name'];
+        $author->save();
+
+        return redirect('/authors');
+    }
+
+    // delete authors
+    public function delete(Author $author)
+    {
+        $author->delete();
+        return redirect('/authors');
+    }
 }
