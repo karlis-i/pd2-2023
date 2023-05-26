0818233d525af52ac4d85cfeefab62a073bf6a70
Kārlis Immers <karlis.im@gmail.com>
2023-05-26 23:32:57 +0300

    Add authentication

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,8 @@ Route::post('/authors/put', [AuthorController::class, 'put']);
 Route::get('/authors/update/{author}', [AuthorController::class, 'update']);
 Route::post('/authors/patch/{author}', [AuthorController::class, 'patch']);
 Route::post('/authors/delete/{author}', [AuthorController::class, 'delete']);
+
+// Auth routes
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/auth', [AuthController::class, 'authenticate']);
+Route::get('/logout', [AuthController::class, 'logout']);
